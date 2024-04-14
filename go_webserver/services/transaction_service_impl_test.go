583d587c78c44988bebc6f681b80f40ce96d0d21
avinash6 <avinash.sg7@gmail.com>
2024-04-14 17:32:48 +0800

    Check error handling during commit

--- go_webserver/services/transaction_service_impl_test.go
+++ go_webserver/services/transaction_service_impl_test.go
@@ -2,6 +2,7 @@ package services
 
 import (
 	"context"
+	"errors"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
 	"testing"
@@ -14,7 +15,14 @@ func TestAddTransaction(t *testing.T) {
 	t.Run("Doesn't return error if transaction is successful", func(t *testing.T) {
 		mockTranRepo, mockAccRepo, mockTran, service, ctx, cancel := initializeMocks()
 		defer cancel()
-		initializeSuccessfulTransaction(mockTran, ctx, mockTranRepo, mockAccRepo)
+		mockTran.On("BeginTransaction", mock.Anything).Return(ctx, nil)
+		mockTranRepo.On("AddTransaction", mock.Anything, mock.Anything).Return(nil)
+		mockAccRepo.On("AddBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+		mockAccRepo.On("DeductBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+		mockTran.On("Commit", mock.Anything).Return(nil)
+
 		err := service.AddTransaction("toAccountID", "fromAccountID", 100.00, ctx)
 		assert.Nil(t, err)
 	})
@@ -48,7 +56,8 @@ func TestAddTransaction(t *testing.T) {
 		defer cancel()
 		mockTran.On("BeginTransaction", mock.Anything).Return(ctx, nil)
 		mockTranRepo.On("AddTransaction", mock.Anything, mock.Anything).Return(nil)
-		mockAccRepo.On("AddBalance", mock.Anything, mock.Anything, mock.Anything).Return(nil)
+		mockAccRepo.On("AddBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
 		mockAccRepo.On("DeductBalance", mock.Anything, mock.Anything, mock.Anything).
 			Return(assert.AnError)
 		mockTran.On("Rollback", mock.Anything).Return(nil)
@@ -60,13 +69,20 @@ func TestAddTransaction(t *testing.T) {
 	t.Run("Commits if no errors are encountered", func(t *testing.T) {
 		mockTranRepo, mockAccRepo, mockTran, service, ctx, cancel := initializeMocks()
 		defer cancel()
-		initializeSuccessfulTransaction(mockTran, ctx, mockTranRepo, mockAccRepo)
+		mockTran.On("BeginTransaction", mock.Anything).Return(ctx, nil)
+		mockTranRepo.On("AddTransaction", mock.Anything, mock.Anything).Return(nil)
+		mockAccRepo.On("AddBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+		mockAccRepo.On("DeductBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+		mockTran.On("Commit", mock.Anything).Return(nil)
+
 		service.AddTransaction("toAccountID", "fromAccountID", 100.00, ctx)
 		mockTran.AssertNumberOfCalls(t, "Commit", 1)
 		mockTran.AssertNumberOfCalls(t, "Rollback", 0)
 	})
 
-	t.Run("Rollsback if errors are encountered", func(t *testing.T) {
+	t.Run("Rollback if errors are encountered", func(t *testing.T) {
 		mockTranRepo, _, mockTran, service, ctx, cancel := initializeMocks()
 		defer cancel()
 		mockTran.On("BeginTransaction", mock.Anything).Return(ctx, nil)
@@ -76,6 +92,24 @@ func TestAddTransaction(t *testing.T) {
 		service.AddTransaction("toAccountID", "fromAccountID", 100.00, ctx)
 		mockTran.AssertNumberOfCalls(t, "Commit", 0)
 		mockTran.AssertNumberOfCalls(t, "Rollback", 1)
+	})
+
+	t.Run("Returns error if error is encountered during commit", func(t *testing.T) {
+		mockTranRepo, mockAccRepo, mockTran, service, ctx, cancel := initializeMocks()
+		defer cancel()
+		mockTran.On("BeginTransaction", mock.Anything).Return(ctx, nil)
+		mockTranRepo.On("AddTransaction", mock.Anything, mock.Anything).Return(nil)
+		mockAccRepo.On("AddBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+		mockAccRepo.On("DeductBalance", mock.Anything, mock.Anything, mock.Anything).
+			Return(nil)
+		mockTran.On("Commit", mock.Anything).Return(errors.New("commit error"))
+
+		err := service.AddTransaction("toAccountID", "fromAccountID", 100.00, ctx)
+		assert.Error(t, err)
+		assert.ErrorContains(t, err, "commit error")
+		mockTran.AssertNumberOfCalls(t, "Commit", 1)
+		mockTran.AssertNumberOfCalls(t, "Rollback", 0)
 	})
 }
 
@@ -94,19 +128,4 @@ func initializeMocks() (
 	service := CreateNewTransactionServiceImpl(mockTranRepo, mockAccRepo, mockTran)
 	addCtx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
 	return mockTranRepo, mockAccRepo, mockTran, service, addCtx, cancel
-}
-
-func initializeSuccessfulTransaction(
-	mockTran *mocks.MockTransactional,
-	addCtx context.Context,
-	mockTranRepo *mocks.MockTransactionRepository,
-	mockAccRepo *mocks.MockAccountRepository,
-) {
-	mockTran.On("BeginTransaction", mock.Anything).Return(addCtx, nil)
-	mockTranRepo.On("AddTransaction", mock.Anything, mock.Anything).Return(nil)
-	mockAccRepo.On("AddBalance", mock.Anything, mock.Anything, mock.Anything).
-		Return(nil)
-	mockAccRepo.On("DeductBalance", mock.Anything, mock.Anything, mock.Anything).
-		Return(nil)
-	mockTran.On("Commit", mock.Anything).Return(nil)
 }
